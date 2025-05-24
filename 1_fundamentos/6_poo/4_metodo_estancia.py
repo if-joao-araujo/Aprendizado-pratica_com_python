9cf8ec048ac95202b7d96f5ac1837d4c059352d5
João pedro santana de araújo <192832386+if-joao-araujo@users.noreply.github.com>
2025-05-24 18:28:06 -0300

    metodos de estancia

---
+++ 1_fundamentos/6_poo/4_metodo_estancia.py
@@ -0,0 +1,31 @@
+class game:
+    def __init__(self, nome = "", ano_lancamento = 0,online = False, nota = 0):
+        self.nome = nome
+        self.ano_lancamento = ano_lancamento
+        self.online = online
+        self.nota = nota
+        self.total_evaluetion = 0
+        self.evaluertors = 0
+
+    def __str__(self):
+       return f"Jogo:{(self.nome)} "
+    
+    def techincal_sheet(self):   
+        print("## Dados do jogo ##")
+        print(f"Nome do jogo: {self.nome}\n Ano de lançamento: {self.ano_lancamento}")
+        print(f"O jogo é multiplayer: {self.online}\n avaliações {self.nota}\n")
+        
+    def evaluete(self,nota):
+        self.total_evaluetion += nota
+        self.evaluertors +=1
+    
+    def avarege(self):
+        print(f"A média do filme {self.nome} == {self.total_evaluetion / self.evaluertors}")
+
+    
+game1 = game("minecraft",2007,True,9.6)
+game2 = game("Fortinite", 2017,True, 7.8)
+game1.techincal_sheet()
+game1.evaluete(9.0)
+game1.evaluete(7.0)
+game1.avarege()
