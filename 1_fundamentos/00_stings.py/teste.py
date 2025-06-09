96dba3287532c054b90e142aef73a324caaa600a
João pedro santana de araújo <if.joao.araujo@gmail.com>
2025-06-09 20:00:39 -0300

    pedra papel e tesoura
    jogo simples, apenas para matar o tédio

---
+++ 1_fundamentos/00_stings.py/teste.py
@@ -0,0 +1,11 @@
+curso = "  curSo de pYtHon  "
+valor = curso 
+
+print(curso is valor)
+print(curso.center(30,"#"))
+print(".".join(curso))
+print(curso.title())
+print(curso.strip())
+print(curso.lstrip())
+print(curso.rstrip())
+print(curso)
